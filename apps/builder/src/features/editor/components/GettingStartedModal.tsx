a0173649e8edff3a472264b03d57b492d55c20aa
Sreejith.R <shrsree341o@gmail.com>
2024-03-17 15:24:11 +0530

    Update youtube

--- apps/builder/src/features/editor/components/GettingStartedModal.tsx
+++ apps/builder/src/features/editor/components/GettingStartedModal.tsx
@@ -130,7 +130,7 @@ export const GettingStartedModal = () => {
             <iframe
               width="100%"
               height="315"
-              src="https://www.youtube.com/embed/jp3ggg_42-M"
+              src="#youtube"
               title="YouTube video player"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
@@ -148,7 +148,7 @@ export const GettingStartedModal = () => {
                   <iframe
                     width="100%"
                     height="315"
-                    src="https://www.youtube.com/embed/6BudIC4GYNk"
+                    src="#youtube"
                     title="YouTube video player"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
@@ -157,7 +157,7 @@ export const GettingStartedModal = () => {
                   <iframe
                     width="100%"
                     height="315"
-                    src="https://www.youtube.com/embed/ZuyDwFLRbfQ"
+                    src="#youtube"
                     title="YouTube video player"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
